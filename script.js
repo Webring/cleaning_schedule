a00b02d7c2429e70592821c73b51cce4279a8014
Mihail <35828413+Webring@users.noreply.github.com>
2023-09-17 15:30:59 +0700

    fix problem with number of weeks

--- script.js
+++ script.js
@@ -1,31 +1,42 @@
 const PEOPLE_LIST = ["Михаил", "Ильдар", "Кирилл", "Егор", "Влад", "Борис"]
 const START_DATE = "19.12.2022"
-const START_POS = 0
+const START_POS = 1
 
-let start_date = moment(START_DATE, "DD.MM.YYYY").locale("ru").startOf("week") + moment.duration(days = 1)
+let start_date = moment(START_DATE, "DD.MM.YYYY").locale("ru").startOf("week")
 let date_selector = document.getElementById("date_select")
 
 update_page()
 
+function get_workers(week) {
+    let result = []
+    for (let i = 0; i < 4; i++) {
+        result.push(PEOPLE_LIST[(START_POS + week + i) % PEOPLE_LIST.length])
+    }
+    return result
+}
+
 function update_page(to_moment = moment(moment().format("YYYY-MM-DD"))) {
     let duration = moment.duration(to_moment - start_date)
-    let number_of_weeks = duration.weeks()
+    let number_of_weeks = Math.floor(duration.asWeeks())
+
+    let workers = get_workers(number_of_weeks)
+    let last_workers = get_workers(number_of_weeks - 1)
 
     document.getElementById("selected_date").innerText = to_moment.format("DD.MM.YYYY");
     document.getElementById("week_start").innerText = to_moment.locale("ru").startOf("week").format("DD.MM.YYYY")
     document.getElementById("week_end").innerText = to_moment.locale("ru").endOf("week").format("DD.MM.YYYY")
 
-    document.getElementById("thursday-clean").innerText = PEOPLE_LIST[(START_POS + number_of_weeks) % PEOPLE_LIST.length]
-    document.getElementById("thursday-wash").innerText = PEOPLE_LIST[(START_POS + number_of_weeks + 1) % PEOPLE_LIST.length]
-    document.getElementById("sunday-clean").innerText = PEOPLE_LIST[(START_POS + number_of_weeks) % PEOPLE_LIST.length]
-    document.getElementById("sunday-wash").innerText = PEOPLE_LIST[(START_POS + number_of_weeks + 1) % PEOPLE_LIST.length]
-    document.getElementById("sunday-toilet").innerText = PEOPLE_LIST[(START_POS + number_of_weeks + 2) % PEOPLE_LIST.length]
-    document.getElementById("sunday-bathroom").innerText = PEOPLE_LIST[(START_POS + number_of_weeks + 3) % PEOPLE_LIST.length]
+    document.getElementById("thursday-clean").innerText = workers[0]
+    document.getElementById("thursday-wash").innerText = workers[1]
+    document.getElementById("sunday-clean").innerText = workers[0]
+    document.getElementById("sunday-wash").innerText = workers[1]
+    document.getElementById("sunday-toilet").innerText = workers[2]
+    document.getElementById("sunday-bathroom").innerText = workers[3]
 
-    document.getElementById("last_floor_sweeper").innerText = PEOPLE_LIST[(START_POS + number_of_weeks - 1) % PEOPLE_LIST.length]
-    document.getElementById("last_floor_washer").innerText = PEOPLE_LIST[(START_POS + number_of_weeks) % PEOPLE_LIST.length]
-    document.getElementById("last_toilet_washer").innerText = PEOPLE_LIST[(START_POS + number_of_weeks + 1) % PEOPLE_LIST.length]
-    document.getElementById("last_bathroom_washer").innerText = PEOPLE_LIST[(START_POS + number_of_weeks + 2) % PEOPLE_LIST.length]
+    document.getElementById("last_floor_sweeper").innerText = last_workers[0]
+    document.getElementById("last_floor_washer").innerText = last_workers[1]
+    document.getElementById("last_toilet_washer").innerText = last_workers[2]
+    document.getElementById("last_bathroom_washer").innerText = last_workers[3]
 }
 
 document.getElementById("show_datepicker_button").addEventListener("click", function () {
@@ -33,7 +44,6 @@ document.getElementById("show_datepicker_button").addEventListener("click", func
 })
 
 date_selector.addEventListener("change", function () {
-    console.log(this.value)
     update_page(moment(this.value));
 })
 
